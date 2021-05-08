9b8e0d36b0fa0720f597767f263419c5117dacd0
Boris Yelnikoff <b.zawada84@gmail.com>
2021-05-08 20:52:16 +0200

    Init node module

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const collapsibles = document.querySelectorAll(".collapsible");
-collapsibles.forEach(item => 
-    item.addEventListener("click", function() {
-        this.classList.toggle("collapsible--expanded");
-    }));
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
